465b66031558cb586541c2b060b70fab46c11018
sokakozos <sokakozos@gmail.com>
2023-02-12 09:23:42 +0100

    v2

--- Debug/Src/subdir.mk
+++ Debug/Src/subdir.mk
@@ -22,7 +22,7 @@ C_DEPS += \
 
 # Each subdirectory must supply rules for building sources it contributes
 Src/%.o Src/%.su: ../Src/%.c Src/subdir.mk
-	arm-none-eabi-gcc "$<" -mcpu=cortex-m4 -std=gnu11 -g3 -DDEBUG -DSTM32F429I_DISC1 -DSTM32 -DSTM32F429ZITx -DSTM32F4 -c -I../Inc -O0 -ffunction-sections -fdata-sections -Wall -fstack-usage -MMD -MP -MF"$(@:%.o=%.d)" -MT"$@" --specs=nano.specs -mfloat-abi=soft -mthumb -o "$@"
+	arm-none-eabi-gcc "$<" -mcpu=cortex-m4 -std=gnu11 -g3 -DDEBUG -DSTM32F429I_DISC1 -DSTM32 -DSTM32F429ZITx -DSTM32F4 -c -I../Inc -O0 -ffunction-sections -fdata-sections -Wall -fstack-usage -MMD -MP -MF"$(@:%.o=%.d)" -MT"$@" --specs=nano.specs -mfpu=fpv4-sp-d16 -mfloat-abi=soft -mthumb -o "$@"
 
 clean: clean-Src
 
--- Src/main.c
+++ Src/main.c
@@ -1,12 +1,36 @@
 
-#include <stdint.h>
-#include "stdio.h"
+#include "main.h"
 
 int main(void)
 {
+	RCC_AHB1ENR_t volatile *const pClkCtrlReg   = (RCC_AHB1ENR_t*) 0x40023830;
+	GPIOx_MODE_t  volatile *const pPortGModeReg = (GPIOx_MODE_t*) 0x40021800;
+	GPIOx_ODR_t   volatile *const pPortGOutReg  = (GPIOx_ODR_t*) 0x40021814;
+
+	//1. enable the clock for GPOIG peripheral in the AHB1ENR
+	pClkCtrlReg->gpiog_en = 1;
 
 
-	return 0;
+	//2. configure the mode of the IO pins as output
+	pPortGModeReg->pin_13 = 1;
+	pPortGModeReg->pin_14 = 1;
+
+	while(1)
+	{
+		//Set 13th and 14th bit of the output data register to make I/O pin-13 and pin-14 as HIGH
+		pPortGOutReg->pin_13  = 1;
+		pPortGOutReg->pin_14  = 0;
+
+
+		//introduce small human observable delay
+		//This loop executes for 300K times
+		for(uint32_t i=0 ; i < 600000 ; i++ );
+
+		//Reset 13th and 14th bit of the output data register to make I/O pin-13 and pin-14 as LOW
+		pPortGOutReg->pin_13  = 0;
+		pPortGOutReg->pin_14  = 1;
+
+		for(uint32_t i=0 ; i < 600000 ; i++ );
+	}
 
 }
-
